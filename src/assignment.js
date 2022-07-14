0289620ea1c5dc19451dd3722db289b03e78e731
Oluwafunmilayo Kalejaye <105063707+FunmiKay@users.noreply.github.com>
2022-07-14 20:04:33 +0000

    I did Challenge 1 and 2 I installed yarn I also pass my yarn test

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+const sumOfNumbers = (arrayOfNumbers) => {
+  let sumOfNum = arrayOfNumbers.reduce((acc,val) =>{
+    return acc+ val;
+  },0)
+  return sumOfNum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+    
+  
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const even = [];
+
+  arrayOfNumbers.forEach(number => {
+    if (number % 2===0){
+      even.push(number);
+    }
+  });
+    return even.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
